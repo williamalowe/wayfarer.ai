201b2fec5c00e27d3fb89006aa5417465588561c
walowe <williamlowedev@gmail.com>
2025-06-30 15:46:01 +1000

    feat: add sign-in page ui

---
+++ src/app/sign-in/new-user/page.tsx
@@ -0,0 +1,11 @@
+
+const NewUserPage = () => {
+  return (
+    <div>
+        <h5 className="text-xl font-bold">Let us get to know you a little better.</h5>
+        
+    </div>
+  )
+}
+
+export default NewUserPage
